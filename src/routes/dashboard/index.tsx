a650cdc5f34cf8e2e873170304243c031dae7ebb
arkhon7 <reviuy9@gmail.com>
2024-04-01 10:29:04 +0800

    update

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -14,6 +14,8 @@ export function Dashboard() {
     null,
   );
 
+  const [viewState, setViewState] = React.useState<"own" | "other">("own");
+
   React.useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/api/user/", {
@@ -23,6 +25,7 @@ export function Dashboard() {
       })
       .then((response) => {
         setUser(response.data);
+
         console.log(response.data);
       })
       .catch((reason) => console.log(reason));
@@ -55,36 +58,69 @@ export function Dashboard() {
   }, [user]);
 
   return (
-    <div className="h-full w-full">
-      <Navbar />
-
-
-      <div>
-        
-
+    <div className="relative h-full w-full">
+      <div className="absolute h-full w-full">
+        <Navbar />
       </div>
 
-      {/*     
+      <div className="flex h-full w-full items-center justify-center">
+        {user !== null && userRecipes !== null && collectedRecipes !== null ? (
+          <div className="h-[70%] w-[65%]">
+            <div className="w-full">
+              <div className="font-paytone-one text-[32px] font-normal">
+                Hi, {user.username}!
+              </div>
+              <div>this is your recipe collection</div>
+            </div>
 
-      <div className="text-10">
-        {user === null ? <div>Loading...</div> : JSON.stringify(user)}
-      </div>
+            <span className="flex p-2">
+              <div>
+                {viewState === "own" ? (
+                  <button
+                    className="bg-10 p-2 font-lexend text-50"
+                    onClick={() => setViewState("own")}
+                  >
+                    Own recipes
+                  </button>
+                ) : (
+                  <button
+                    className="bg-50 p-2 font-lexend text-40"
+                    onClick={() => setViewState("own")}
+                  >
+                    Own Recipes
+                  </button>
+                )}
+              </div>
 
-      <div className="text-20">
-        {userRecipes === null ? (
-          <div>Loading...</div>
+              <div>
+                {viewState === "other" ? (
+                  <button
+                    className="bg-10 p-2 font-lexend text-50"
+                    onClick={() => setViewState("other")}
+                  >
+                    Other recipes
+                  </button>
+                ) : (
+                  <button
+                    className="bg-50 p-2 font-lexend text-40"
+                    onClick={() => setViewState("other")}
+                  >
+                    Other Recipes
+                  </button>
+                )}
+              </div>
+            </span>
+
+            <div>
+              {userRecipes.results.forEach((element: any) => (
+                <div>{element}</div>
+              ))}
+            </div>
+          </div>
         ) : (
-          JSON.stringify(userRecipes)
+          <div>Loading...</div>
         )}
       </div>
-
-      <div className="text-30">
-        {collectedRecipes === null ? (
-          <div>Loading...</div>
-        ) : (
-          JSON.stringify(collectedRecipes)
-        )}
-      </div> */}
     </div>
   );
 }
